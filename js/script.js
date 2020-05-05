72e567330a1f0c5a3592f132ec0f8bcbf702065a
Darius M <darmis@darmis.lt>
2020-05-05 16:29:07 +0300

    Memory game for kodinu.lt challange

---
+++ js/script.js
@@ -0,0 +1,88 @@
+let firstCardFront = '';
+        let firstCardBack = '';
+        let secondCardFront = '';
+        let secondCardBack = '';
+        let total = 0;
+
+        const cards = document.querySelectorAll('.card');
+        const resetBtn = document.getElementById('btn-reset');
+        const modalResetBtn = document.getElementById('modal-reset-btn');
+        
+        resetBtn.addEventListener('click', function() {
+            resetButton();
+        });
+
+        modalResetBtn.addEventListener('click', function() {
+            resetButton();
+            $('#modal').modal('hide');
+        });
+
+        (function shuffle() {
+            shuffleCards()
+        })();
+
+        function resetButton() {
+            total = 0;
+            resetVariables();
+            shuffleCards();
+
+            let fronts = document.querySelectorAll('.front');
+            for(let i=0; i<fronts.length; i++){
+                fronts[i].classList.remove('d-none');
+            }
+
+            let backs = document.querySelectorAll('.back');
+            for(let i=0; i<backs.length; i++){
+                backs[i].classList.remove('d-none');
+                backs[i].classList.add('d-none');
+            }
+        }
+
+        function shuffleCards() {
+            cards.forEach((card) => {
+                let randomPosition = Math.floor(Math.random() * 16);
+                card.style.order = randomPosition;
+            });
+        }
+
+        function resetVariables() {
+            firstCardFront = '';
+            firstCardBack = '';
+            secondCardFront = '';
+            secondCardBack = '';
+        }
+
+        function compareCards(event) {
+            let selectedBackCard = event.target;
+            selectedBackCard.classList.add('d-none');
+            let selectedFrontCard = event.target.nextElementSibling;
+            selectedFrontCard.classList.remove('d-none');
+            
+            if (secondCardBack == '' && firstCardBack != '') {
+                secondCardBack = selectedBackCard;
+                secondCardFront = selectedFrontCard;
+            }
+
+            if (firstCardBack == '' && secondCardBack == '') {
+                firstCardBack = selectedBackCard;
+                firstCardFront = selectedFrontCard;
+            }
+
+            if (firstCardBack != '' && secondCardBack != '') {
+                if (firstCardFront.src != secondCardFront.src) {
+                    setTimeout(function() {
+                        firstCardFront.classList.add('d-none');
+                        firstCardBack.classList.remove('d-none');
+                        secondCardFront.classList.add('d-none');
+                        secondCardBack.classList.remove('d-none');
+                        resetVariables();
+                    }, 300);
+                } else {
+                    total++;
+                    resetVariables();
+                    if (total === 8) {
+                        $('#modal').modal('show');
+                    }
+                }
+            }
+        }
